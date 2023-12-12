3a548d2854caafb533d5472fd5966af7ea650169
King Nguyen <NguyenQuocVuong-git>
2023-12-12 17:13:00 +0700

    feat: config socket, get info onother user, save message

--- user-infomation-v2/src/main.js
+++ user-infomation-v2/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
+import store from './store/store';
+import router from './routes/router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(vuetify);
+app.use(store);
+app.use(router);
+
+loadFonts();
+
+app.mount('#app');
